2f1cff5c0fa71fcc6c5f02899dae4f2029db7c43
YT <1843438083@qq.com>
2021-09-15 12:05:59 +0800

    maven -  zongjie

---
+++ review_code/src/review/gk/exam/XC.java
@@ -0,0 +1,31 @@
+package review.gk.exam;
+
+import java.util.*;
+public class test {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        char[] chars = str.toCharArray();
+        Map<String,Integer> countMap = new HashMap<>();
+        countMap.put("letter",0);
+        countMap.put("number",0);
+        countMap.put("space",0);
+        countMap.put("other",0);
+        for(char c : chars){
+            if((c>='a'&&c<='z')||(c>='A'&&c<='Z')){
+                countMap.put("letter",countMap.get("letter")+1);
+            }else if(c>='0' && c <='9'){
+                countMap.put("number",countMap.get("number")+1);
+            }else if(c == ' '){
+                countMap.put("space",countMap.get("space")+1);
+            }else{
+                countMap.put("other",countMap.get("other")+1);
+            }
+        }
+        for(String key : countMap.keySet()){
+            System.out.println(countMap.get(key));
+        }
+    }
+
+}
